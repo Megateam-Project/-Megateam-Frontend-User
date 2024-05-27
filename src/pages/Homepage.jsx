a9d605d466cde9aaaeda6969aa17b78e6cefd389
bon bon <bona35165@gmail.com>
2024-05-27 23:14:48 +0700

    homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,25 @@
-export function Homepage(){
-    return (
-        <div>   Homepage</div>
-    )
+import Rectangle from "../assets/Rectangle 5.png";
+export function Homepage() {
+  return (
+    <div>
+      <div>
+        <div className="row">
+          <div className="cl-6 ">
+            <img className="image" src={Rectangle} alt="image" />
+          </div>
+          <div className="cl-6">
+            <p>Paradise View</p>
+            <h1>Hotel for every moment rich in emotion</h1>
+            <p>Every moment feels like the first time in paradise view</p>
+            <button>Book Now</button>
+          </div>
+            
+        </div>
+        
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
