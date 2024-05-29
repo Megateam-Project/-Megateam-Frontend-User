e83165460cf21f30a2e038beb73b4659ddbd644b
unknown <admin@pnvl199.pn.com.vn>
2024-05-29 18:21:39 +0700

    CreateStruction

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,3 +1,15 @@
-export function Room(){
-    return <div>Room</div>
+import { Link } from "react-router-dom";
+import './Detail/DetailRoom.jsx' ;
+export function Room() {
+  return (
+    <div>
+      Room
+      <div>
+        <h1>Single room </h1>
+        <button >
+        <Link to="/detail" className="nav-link fs-4">Show detail</Link>
+        </button>
+      </div>
+    </div>
+  );
 }
