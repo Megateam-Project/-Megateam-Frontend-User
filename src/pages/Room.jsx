a1c80a238ada5202a2505ad8134a396630f6e08a
Khánh Nhi <nhi.tuong25@student.passerellesnumeriques.org>
2024-06-04 09:38:57 +0700

    Nhi/done/feat/deatil

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,15 +1,5 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // import './Detail/DetailRoom.jsx' ;
 export function Room() {
-  return (
-    <div>
-      Room
-      <div>
-        <h1>Single room </h1>
-        <button >
-        <Link to="/detail" className="nav-link fs-4">Show detail</Link>
-        </button>
-      </div>
-    </div>
-  );
+  return 
 }
